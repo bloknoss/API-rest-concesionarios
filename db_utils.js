ae04978d10cfd13bedfded488309595bc647d36b
bloknoss <windpourwow@gmail.com>
2023-12-04 22:29:26 +0100

    Añado funciones para actualizar el concesionario y parsear la ID

--- db_utils.js
+++ db_utils.js
@@ -63,3 +63,52 @@ async function dbDeleteOne(id) {
         conn.close();
     }
 }
+
+async function dbUpdateConcesionario(id, nombre, direccion, coches) {
+    const conn = new MongoClient(`${db_uri}/${db_name}`);
+    try {
+        const collection = conn.db().collection(collection_name);
+
+        const results = await collection.updateOne(
+            { _id: id},
+            {
+                $set: {
+                    nombre: nombre,
+                    direccion: direccion,
+                    coches: coches,
+                },
+            }
+        );
+
+        return results;
+    } catch (err) {
+    } finally {
+        conn.close();
+    }
+}
+
+async function parseObjectId(id) {
+    const conn = new MongoClient(`${db_uri}/${db_name}`);
+
+    try {
+        const collection = conn.db().collection(collection_name);
+
+        const results = await collection.find().toArray();
+
+        //Validacion de la ID proporcionada
+        if (isNaN(id)) {
+            id = new ObjectId(id);
+            let existsId = await collection.findOne({ _id: id });
+
+            return existsId == null ? { message: "No existe la Id de Concesionario que has introducido." } : existsId._id;
+        } else if (results.length < parseInt(id) + 1) {
+            return Promise.resolve({ message: "Concesionario not found" });
+        } else {
+            return results[parseInt(id)]._id;
+        }
+    } catch (err) {
+        console.log(err);
+    } finally {
+        conn.close();
+    }
+}
