cbd076738ed3ad4e4fcb2286b8a3aded88e11b15
bloknoss <windpourwow@gmail.com>
2023-12-08 17:15:00 +0100

    Configurados los routers

--- routes/concesionariosRouter.js
+++ routes/concesionariosRouter.js
@@ -1,3 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+const {getConcesionario, getConcesionarios, updateConcesionario, deleteConcesionario, createConcesionario} = require('../controllers/concesionariosControllers');
+
+router.get('/concesionarios', getConcesionarios);
+router.post('/concesionarios', createConcesionario);
+router.get('/concesionarios/:id/',getConcesionario);
+router.put('/concesionarios/:id', updateConcesionario);
+router.delete('/concesionarios/:id', deleteConcesionario);
+
+module.exports = router;
