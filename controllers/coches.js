77e90a769c2fbec824579e1d594854f762b192a2
bloknoss <windpourwow@gmail.com>
2023-12-09 18:02:48 +0100

    Control de errores en coches.js

--- controllers/coches.js
+++ controllers/coches.js
@@ -2,49 +2,72 @@ const cochesService = require('../services/coches');
 
 // Devuelve todos los coches pertenecientes a un concesionario
 const getCoches =  async (request, response, next) => {
-  const id = request.params.id;
-
-  await cochesService.getCoches(id).then(x => response.json(x));
-  next();
+  
+  try  {
+    const id = request.params.id;
+    await cochesService.getCoches(id).then(x => response.json(x));
+    next();
+  } catch (err) {
+    console.log(`Ha ocurrido un error${err.message}`);
+  }
 };
 
 
 const createCoche = async (request, response, next) => {
-  const id = request.params.id;
-  
-  await cochesService.postCoche(id,request.body); 
-  response.json({ message: 'OK' });
-  next();
+  try {
+    const id = request.params.id;    
+    await cochesService.postCoche(id,request.body);
+
+    response.json({ message: 'OK' });
+    next();
+  } catch (err) {
+    console.log(`Ha ocurrido un error${err.message}`);
+  }
 };
   
 // Obtener un solo coche de un concesionario
 const getCoche = async (request, response, next) => {
-  const id = await request.params.id;
-  const cocheId = parseInt(request.params.cocheId);
-  
-  await cochesService.getCoche(id,cocheId).then(x => response.json(x));
-  next();
+  try {
+
+    const id = await request.params.id;
+    const cocheId = parseInt(request.params.cocheId);
+    
+    await cochesService.getCoche(id,cocheId).then(x => response.json(x));
+    next();
+
+  } catch (err) {
+    console.log(`Ha ocurrido un error: ${err.message}`);
+  }
 };
   
 
 // Actualizar un solo coche perteneciente a un concesionario
 const updateCoche =  async (request, response, next) => {
-  const id =  request.params.id;
-  const cocheId = request.params.cocheId;
-  await cochesService.putCoche(id, cocheId, request.body);
+  try {
+    const id =  request.params.id;
+    const cocheId = request.params.cocheId;
+    await cochesService.putCoche(id, cocheId, request.body);
   
-  response.json({ message: 'OK' });
-  next();
+    response.json({ message: 'OK' });
+    next();
+  } 
+  catch (err) {
+    console.log(`Ha ocurrido un error: ${err.message}`);
+  }
 };
   
 // Borrar un coche en especifico
 const deleteCoche = async (request, response, next) => {
-  const id = request.params.id;
-  const cocheId = request.params.cocheId;
+  try {
+    const id = request.params.id;
+    const cocheId = request.params.cocheId;
   
-  await cochesService.deleteCoche(id, cocheId);
+    await cochesService.deleteCoche(id, cocheId);
 
-  response.json({ message: 'OK' });
+    response.json({ message: 'OK' });
+  } catch (err) {
+    console.log(`Ha ocurrido un error: ${err.message}`);
+  }
 }; 
 
 
--- controllers/concesionarios.js
+++ controllers/concesionarios.js
@@ -4,39 +4,65 @@ const concesionarioService =  require('../services/concesionarios');
 
 // Lista todos los concesionarios
 const getConcesionarios = async (_request, response,next) => {
-  await concesionarioService.getConcesionarios().then(x => response.json(x));
-  next();
+  try {
+    await concesionarioService.getConcesionarios().then(x => response.json(x));
+    next();
+  } catch (err) {
+    console.log(`Ha ocurrido un error: ${  err.message}`);
+  }
 };
   
 // Lista los concesionarios
 const createConcesionario = async (request, response,next) => {
 
-  await concesionarioService.postConcesionario(request.body);
-  
-  response.json({ message: 'OK' });
-  next();
+  try {
+    await concesionarioService.postConcesionario(request.body);
+    
+    response.json({ message: 'OK' });
+    next();
+
+  } catch (err) {
+
+    console.log(`Ha ocurrido un error: ${  err.message}`);
+  }
 };
   
 // Obtener un solo concesionario
 const getConcesionario = async (request, response,next) => {
-  const id = request.params.id;
-  await concesionarioService.getConcesionario(id).then(x => response.json(x));
+  try {
+    
+    const id = request.params.id;
+    await concesionarioService.getConcesionario(id).then(x => response.json(x));
+  } catch (err) {
+
+    console.log(`Ha ocurrido un error: ${  err.message}`);
+  }
+
 };
   
 // Actualizar un solo concesionario
 const updateConcesionario =  async (request, response,next) => {
-  const id = request.params.id;
-  await concesionarioService.putConcesionario(id, request.body);
-  response.json({ message: 'OK' });
-  next();
+  try {
+    const id = request.params.id;
+    await concesionarioService.putConcesionario(id, request.body);
+    response.json({ message: 'OK' });
+    next();
+  } catch (err) {
+    console.log(`Ha ocurrido un error: ${  err.message}`);
+  }
 };
   
 // Borrar un solo concesionario
 const deleteConcesionario  = async (request, response,next) => {
-  const id = request.params.id;
-  await concesionarioService.deleteConcesionario(id);
-  response.json({ message: 'OK' });
-  next();
+  try {
+    const id = request.params.id;
+    await concesionarioService.deleteConcesionario(id);
+    response.json({ message: 'OK' });
+    next();
+
+  } catch (err) {
+    console.log(`Ha ocurrido un error: ${  err.message}`);
+  }
 };
 
 
